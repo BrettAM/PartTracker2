70bd9b5c90950d596d1902321eeb19e94c9685e9
Brett Menzies <scimor5@gmail.com>
2016-06-01 20:49:29 -0700

    Some new libraries

--- build.sbt
+++ build.sbt
@@ -1,2 +1,3 @@
 libraryDependencies += "com.sparkjava" % "spark-core" % "2.5"
 libraryDependencies += "org.slf4j" % "slf4j-simple" % "1.7.21"
+libraryDependencies += "com.google.code.gson" % "gson" % "2.6.2"
--- main.scala
+++ main.scala
@@ -1,6 +1,10 @@
-import spark._;
-import spark.Spark;
-import spark.Spark._;
+import spark._
+import spark.Spark
+import spark.Spark._
+
+import com.google.gson.Gson
+
+import scala.io.Source
 
 object Main {
 	type Route_t = (Request,Response)=>AnyRef
@@ -12,47 +16,48 @@ object Main {
 		def handle(q: Request, p: Response): AnyRef = f(q,p)
 	}
 
+	def jobsPage = {
+		Source.fromFile("./public/count.html").getLines.mkString
+	}
+
 	val counts = scala.collection.mutable.HashMap.empty[String,Int]
 
+	case class Job(val id: Int, val target: Int) {
+		var count: Int = 0
+	}
+	val gson = new Gson();
+
 	def main(args: Array[String]) = {
-		staticFiles.externalLocation("./public");
-		post("/start"){ (req,res) =>
-			val jid = req.queryParams("jid")
-			println("Start request "+jid)
-			if(jid != null){
-				println("jid not null")
-				req.session(true)
-				req.session.attribute("jid",jid)
-				if(!counts.contains(jid)){
-					counts(jid) = 0
-				}
-				"count.html"
-			} else {
-				"error.html"
+		val job = Job(5, 10)
+		val json = gson.toJson(job)
+		println()
+		println(job)
+		println(json)
+		println(gson.fromJson(json,classOf[Job]))
+		println()
+
+		staticFiles.externalLocation("./public")
+		Spark.port(4567)
+
+		get("/jobs"){ (req,res) =>
+			val id: String = req.queryParams("id")
+			println("jobs request id "+id)
+			if(!counts.contains(id)){
+				counts(id) = 0
 			}
+			jobsPage
 		}
-		post("/up"){ (req,res) =>
-			//println("up "+req.session.attributes())
-			val id: String = req.session.attribute("jid")
-			println("Up from "+id)
-			if(counts.contains(id)){
-				counts(id) += 1
-				counts(id).toString()
-			} else {
-				halt()
-				""
-			}
-		}
-		post("/down"){ (req,res) =>
-			val id: String = req.session.attribute("jid")
-			println("Down from "+id)
-			if(counts.contains(id)){
-				counts(id) -= 1
-				counts(id).toString()
-			} else {
-				halt()
-				""
-			}
+
+		post("/jobs"){ (req,res) =>
+			val dir: String = req.body()
+			val id: String = req.queryParams("id")
+
+			if(dir == "up") counts(id) += 1
+			else if(dir == "down") counts(id) -= 1
+
+			println(id+" : "+dir+" : "+counts(id))
+
+			counts(id).toString()
 		}
 
 		System.console().readLine()
